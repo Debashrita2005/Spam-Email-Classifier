98bfb3d2ff351f79dcc9766086d7795d8556c964
Debashrita Mandal <mandaldebashrita@gmail.com>
2026-01-03 13:01:15 +0530

    Initialize React application in index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+### 6. `src/index.js`
+```javascript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    
+  
+);
+```
+
+---
